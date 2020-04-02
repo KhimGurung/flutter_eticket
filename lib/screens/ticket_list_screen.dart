a8680296458314f79214d18ca950d04bbca740b4
Khim-Bahadur Gurung <khimbahadurgurung@OBCCAdmins-MacBook-Pro.local>
2020-04-03 01:31:29 +0200

    open qr code in a dialog box on click from ticket list screen

--- lib/screens/ticket_list_screen.dart
+++ lib/screens/ticket_list_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'dart:convert';
 import 'package:fluttereticket/icons/flutter_icon_icons.dart';
 import 'package:fluttereticket/models/tickets.dart';
 import 'package:qr_flutter/qr_flutter.dart';
@@ -230,25 +231,60 @@ class _TicketListScreenState extends State<TicketListScreen> {
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        Stack(
-                          overflow: Overflow.visible,
-                          children: <Widget>[
-                            Opacity(
-                              opacity: ticket.valid ? 1 : 0.5,
-                              child: QrImage(
-                                data: 'eticket',
-                                version: QrVersions.auto,
-                                size: 67,
-                                gapless: false,
-                              ),
+                        Material(
+                          child: InkWell(
+                            onTap: (){
+                              if(ticket.valid == true)
+                              showDialog(
+                                context: context,
+                                builder: (_){
+                                  return Dialog(
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(13)
+                                    ),
+                                      child: Container(
+                                        height: 350,
+                                        width: MediaQuery.of(context).size.width,
+                                        child: Column(
+                                          mainAxisAlignment: MainAxisAlignment.center,
+                                          children: <Widget>[
+                                            QrImage(
+                                              data: jsonEncode(ticket),
+                                              version: QrVersions.auto,
+                                              size: 230,
+                                              gapless: false,
+                                            ),
+                                            Text(
+                                              "Scan QR Code"
+                                            )
+                                          ],
+                                        ),
+                                      )
+                                  );
+                                }
+                              );
+                            },
+                            child: Stack(
+                              overflow: Overflow.visible,
+                              children: <Widget>[
+                                Opacity(
+                                  opacity: ticket.valid ? 1 : 0.5,
+                                  child: QrImage(
+                                    data: 'eticket',
+                                    version: QrVersions.auto,
+                                    size: 67,
+                                    gapless: false,
+                                  ),
+                                ),
+                                if(ticket.valid == false)
+                                  Positioned(
+                                      top: -9,
+                                      left: -5,
+                                      child: _renderTicketStatus(ticket.used)
+                                  )
+                              ],
                             ),
-                            if(ticket.valid == false)
-                              Positioned(
-                                  top: -9,
-                                  left: -5,
-                                  child: _renderTicketStatus(ticket.used)
-                              )
-                          ],
+                          ),
                         ),
                         Text(
                           "N0. "+ticket.ticketId,
